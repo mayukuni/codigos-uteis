1b1fcb85c9600627a27137109766659eff51e74d
mayukuni <mayuukuni@gmail.com>
2023-03-06 22:11:21 -0300

    exercicios de introducao feitos no beecrowd

--- exercicios.py
+++ exercicios.py
@@ -1,1 +1,36 @@
+# 1
+print("Hello World!")
 
+# 2
+A = int(input())
+B = int(input())
+X = A + B
+
+print("X =", X)
+
+# 3
+raio = float(input())
+area = 3.14159 * (raio ** 2)
+
+print("A={:.4f}".format(area))
+
+# 4
+A = int(input())
+B = int(input())
+SOMA = A + B
+
+print("SOMA =", SOMA)
+
+# 5
+A = int(input())
+B = int(input())
+PROD = A * B
+
+print("PROD =", PROD)
+
+# 6
+A = float(input())
+B = float(input())
+MEDIA = ((A * 3.5) + (B * 7.5)) / 11
+
+print("MEDIA = {:.5f}".format(MEDIA))
