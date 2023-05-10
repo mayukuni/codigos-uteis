639d22cb8d12d5984c97d36791ef953f64d58cb0
mayukuni <mayuukuni@gmail.com>
2023-05-10 20:51:49 -0300

    inicio da ac4

---
+++ ac4.py
@@ -0,0 +1,53 @@
+# # 1166
+# num1 = int(input())
+# num2 = int(input())
+# pago = 0
+# divida = num1
+# cont = 1
+
+# while pago < num1:
+#     pago = pago + num2
+#     print("pagamento:", cont)
+#     print("antes =", divida)
+#     divida = divida - num2
+#     if num1 - pago < 0:
+#         print("depois = 0")
+#     else:
+#         print("depois =", num1 - pago)
+#     print("-----")
+#     cont = cont + 1
+
+# # 1270
+# num1 = int(input())
+# num2 = int(input())
+# if num1 > num2:
+#     print("Nenhuma tabuada no intervalo!")
+# while num1 <= num2:
+#     vezes = 1
+#     while vezes <= 10:
+#         print(num1, "x", vezes, "=", num1 * vezes)
+#         vezes += 1
+#     num1 += 1
+#     print("----------")
+
+# num1 = int(input())
+# num2 = int(input())
+# if num1 > num2:
+#     print("Nenhuma tabuada no intervalo!")
+# while num1 <= num2:
+#     for i in range(1, 11):
+#         print(f'{num1} x {i} = {num1 * i}')
+#     num1 += 1
+#     print("----------")
+
+# # 1168
+num1 = int(input())
+num2 = int(input())
+
+
+
+
+
+
+
+
