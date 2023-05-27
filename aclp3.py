f7a716de1af83e40564874a3bea9945b760a4947
mayukuni <mayuukuni@gmail.com>
2023-05-27 20:17:02 -0300

    ac3" "

---
+++ aclp3.py
@@ -0,0 +1,66 @@
+# print("a")
+# n = int(input())
+# while n < 1 or n > 50:
+#     print("n invalido")
+#     n = int(input())
+# print("b")
+
+# print("a")
+# limite = int(input())
+# i = 0
+# while i <= limite:
+#     print(i)
+#     i += 2
+# # 10
+# # 0, 2, 4, 6, 8, 10
+
+# soma = 0
+# cont = 1
+# while cont <= 5:
+#     num = int(input())
+#     soma = soma + num
+#     cont += 1
+# print(soma)
+# # 7, 2, 1, 0, 4
+# # 14
+
+# print("limite:")
+# limite = int(input())
+# i = 0
+# while i <= limite:
+#     if i % 2 != 0:
+#         print(i)
+#     i += 1
+# # 13
+# # 1, 3, 5, 7, 9, 11, 13 
+
+# i = 10
+# while i >= 0:
+#     print(i)
+#     i -= 1
+# print("fim")
+# # 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, fim
+
+# def somatemp():
+#     soma = 0
+#     cont = 1
+#     while cont <= 3:
+#         temp = float(input())
+#         soma = soma + temp
+#         cont += 1
+#     return soma
+
+# S = somatemp()
+# print(S)
+# # 10, 20, 30
+# # 60.0
+
+print("informe o numero de 1 a 50:")
+n = int(input())
+while n < 1 or n > 50:
+    print("n invalido")
+    n = int(input())
+print("uhu")
+
+
+
