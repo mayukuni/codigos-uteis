cf488709cb7189394663bf1cb076be2d8b9982ae
Mayumi Kuniyoshi <cinthya.m.kuniyoshi@gmail.com>
2024-10-01 12:27:25 -0300

    adiciona metodos de busca

--- pymongo/models/repository/teste_repository.py
+++ pymongo/models/repository/teste_repository.py
@@ -1,3 +1,4 @@
+from bson.objectid import ObjectId
 from typing import Dict, List
 
 
@@ -15,4 +16,56 @@ class testeRespository:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_many(list_of_documents)
         return list_of_documents
-# oi
+
+    def select_many(self, filter) -> List[Dict]:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        objetos = collection.find(
+            filter,  # o que eu quero que busque
+            {"_id": 0}  # filtro de chaves de valores que vão aparecer, 0 é false e 1 é true
+        )
+
+        response = []
+        for objeto in objetos:
+            response.append(objeto)
+        return response
+
+    def select_one(self, filter) -> Dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        response = collection.find_one(filter, {"_id": 0})
+        return response
+
+    def select_if_property_exists(self, filter) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        objetos = collection.find(filter, {"_id": 0})
+        response = []
+        for objeto in objetos:
+            response.append(objeto)
+        return response
+
+    def select_many_order(self, filter) -> List[Dict]:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        objetos = collection.find(
+            filter,  # o que eu quero que busque
+            {"_id": 0}  # filtro de chaves de valores que vão aparecer, 0 é false e 1 é true
+        ).sort([("idade", 1)])  # ordena pelo valor da idade, -1 é decrescente e 1 é crescente
+
+        response = []
+        for objeto in objetos:
+            response.append(objeto)
+        return response
+
+    def select_or(self, filter) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        objetos = collection.find(filter, {"_id": 0})
+        response = []
+        for objeto in objetos:
+            response.append(objeto)
+        return response
+    
+    def select_by_object_id(self) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        objetos = collection.find({"_id": ObjectId("66fc07aed23912c13056b50c")})
+        response = []
+        for objeto in objetos:
+            response.append(objeto)
+        return response
--- pymongo/teste.py
+++ pymongo/teste.py
@@ -11,16 +11,41 @@ db_connection = db_handle.get_db_connection()
 teste_repository = testeRespository(db_connection)
 
 exemplo = {
-    "nome": "vocee"
+    "nome": "vocee",
+    "idade": "6"
 }
 
-teste_repository.insert_document(exemplo)
+# teste_repository.insert_document(exemplo)
 
 lista_exemplo = [
     {"nome": "vocee"},
-    {"nome": "tu"},
+    {"nome": "tu",
+     "idade": "2"},
     {"nome": "ele"},
-    {"nome": "aaa"},
+    {"nome": "aaa",
+     "cores": {
+         "azul": 1,
+         "rosa": "3"
+     }},
 ]
 
-teste_repository.insert_list_of_documents(lista_exemplo)
+# teste_repository.insert_list_of_documents(lista_exemplo)
+
+result = teste_repository.select_many({"nome": "vocee"})
+print(result)
+
+result2 = teste_repository.select_one({"nome": "vocee"})
+# print(result2)
+
+result3 = teste_repository.select_if_property_exists({"idade": {"$exists": True}})
+# print(result3)
+
+result4 = teste_repository.select_many_order({"nome": "vocee"})
+print()
+print(result4)
+
+result5 = teste_repository.select_or({"$or": [{"nome": "tu"}, {"idade": {"$exists": True}}]})
+print("vaaa", result5)
+
+result6 = teste_repository.select_by_object_id()
+print("ultimo", result6)
